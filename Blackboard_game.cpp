00a128a423f647f85dd699998d22884d83fcee67
Sheikh Samiya Samad Joya <iubatjoya@gmail.com>
2025-07-20 22:29:58 +0600

    Create  Blackboard_game.cpp

---
+++ Blackboard_game.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{ int n,i,x;
+cin>>n;
+for(i=1;i<=n;i++)
+{ cin>>x;
+    if(x%4==0)
+{cout<<"Bob"<<endl;}
+else
+       {cout<<"Alice"<<endl;}}
+}
