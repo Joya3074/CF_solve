65edff232b3704316530625b6b1eca045ad52cb7
Sheikh Samiya Samad Joya <iubatjoya@gmail.com>
2025-07-22 21:08:53 +0600

    Create Easy_problem.cpp

---
+++ Easy_problem.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+ int main()
+{
+  int x;
+  cin >> x;
+  for (int i=0; i<x; i++)
+  {
+    int n;
+    cin >> n;
+    cout << n-1 << endl;
+  }
+    return 0;
+}
